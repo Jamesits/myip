52ee8bdf2f6a000907095daba346c43c12bba2c6
James Swineson <github@public.swineson.me>
2018-12-07 19:28:10 +0800

    make OpenDNS DNS query work

--- src/opendns-dns.go
+++ src/opendns-dns.go
@@ -7,56 +7,58 @@ import (
 	"net"
 )
 
-func dialContextIPv4(ctx context.Context, network, address string) (net.Conn, error) {
-	switch network {
-	case "tcp", "tcp4", "tcp6":
-		network = "tcp4"
-	case "udp", "udp4", "udp6":
-		network = "udp4"
-	}
-
-	return (&net.Dialer{}).DialContext(ctx, network, address)
-}
-
-func dialContextIPv6(ctx context.Context, network, address string) (net.Conn, error) {
-	switch network {
-	case "tcp", "tcp4", "tcp6":
-		network = "tcp6"
-	case "udp", "udp4", "udp6":
-		network = "udp6"
-	}
-	return (&net.Dialer{}).DialContext(ctx, network, address)
-}
-
-func dialContextDualStack(ctx context.Context, network, address string) (net.Conn, error) {
-	return (&net.Dialer{DualStack: true}).DialContext(ctx, network, address)
-}
-
-func OpenDnsDnsQuery(mode Mode, server string) (net.IP, error) {
-	ctx := context.Background()
-
+func dialContextFactory(mode Mode, server string) func(context.Context, string, string) (net.Conn, error) {
 	defaultAddresses := []string{
 		"resolver1.opendns.com:53",
 		"resolver2.opendns.com:53",
 	}
-	n := rand.Int() % len(defaultAddresses)
+	n := rand.Intn(len(defaultAddresses))
 
 	apiEndpoint := server
 	if apiEndpoint == "-" {
 		apiEndpoint = defaultAddresses[n]
 	}
 
+	var ret func(context.Context, string, string) (net.Conn, error)
+	switch mode {
+	case MODE_IPv4:
+		ret = func(ctx context.Context, network, address string) (net.Conn, error) {
+			switch network {
+			case "tcp", "tcp4", "tcp6":
+				network = "tcp4"
+			case "udp", "udp4", "udp6":
+				network = "udp4"
+			}
+
+			return (&net.Dialer{}).DialContext(ctx, network, apiEndpoint)
+		}
+	case MODE_IPv6:
+		ret = func(ctx context.Context, network, address string) (net.Conn, error) {
+			switch network {
+			case "tcp", "tcp4", "tcp6":
+				network = "tcp6"
+			case "udp", "udp4", "udp6":
+				network = "udp6"
+			}
+			return (&net.Dialer{}).DialContext(ctx, network, apiEndpoint)
+		}
+	default:
+		ret = func(ctx context.Context, network, address string) (net.Conn, error) {
+			return (&net.Dialer{DualStack: true}).DialContext(ctx, network, apiEndpoint)
+		}
+	}
+
+	return ret
+}
+
+func OpenDnsDnsQuery(mode Mode, server string) (net.IP, error) {
+	ctx := context.Background()
+
 	resolver := &net.Resolver{
 		PreferGo: true,
 	}
-	switch mode {
-	case MODE_IPv4:
-		resolver.Dial = dialContextIPv4
-	case MODE_IPv6:
-		resolver.Dial = dialContextIPv6
-	default:
-		resolver.Dial = dialContextDualStack
-	}
+
+	resolver.Dial = dialContextFactory(mode, server)
 
 	ips, err := resolver.LookupIPAddr(ctx, "myip.opendns.com")
 	if err != nil {
